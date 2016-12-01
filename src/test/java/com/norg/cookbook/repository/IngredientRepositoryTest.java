c20e014e76950d5fd4983a511c81dd36049af923
SonicNorg <pavel.proger@gmail.com>
2016-12-01 14:01:20 +0700

    Тест исправлен (с использованием Assert) Добавлены тесты удаления ингредиента

--- src/test/java/com/norg/cookbook/repository/IngredientRepositoryTest.java
+++ src/test/java/com/norg/cookbook/repository/IngredientRepositoryTest.java
@@ -1,30 +1,53 @@
 package com.norg.cookbook.repository;
 
 import com.norg.cookbook.model.Ingredient;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Collection;
 
 /**
  * Тесты репозитория ингредиента
  */
 public class IngredientRepositoryTest {
-    private ApplicationContext context;
+    private static ApplicationContext context;
+    private IngredientRepository dao;
 
+    @BeforeClass
+    public static void init() {
+        context = new ClassPathXmlApplicationContext("spring-conf.xml");
+    }
     @Before
     public void getSpringContext() {
-        context = new ClassPathXmlApplicationContext("spring-conf.xml");
+        dao = (IngredientRepository) context.getBean("ingredientRepository");
+        dao.deleteAll();
     }
 
     @Test
     public void addTest() {
-        IngredientRepository dao = (IngredientRepository) context.getBean("ingredientRepository");
-        Ingredient potato = dao.save(new Ingredient("Potato"));
-        Ingredient tomato = dao.save(new Ingredient("Tomato"));
-        System.out.println(dao.findOne(potato.getId()));
-        System.out.println(dao.findOne(tomato.getId()));
+        dao.save(new Ingredient("Potato"));
+        dao.save(new Ingredient("Tomato"));
+        Assert.assertEquals("Count check", 2, dao.count());
+    }
 
+    @Test
+    public void delByInstanceTest() {
+        Ingredient potato = dao.save(new Ingredient("Potato"));
+        Assert.assertEquals("Count check", 1, dao.count());
+        dao.delete(potato);
+        Assert.assertEquals("Count check", 0, dao.count());
+    }
+
+    @Test
+    public void delByIdTest() {
+        Ingredient potato = dao.save(new Ingredient("Potato"));
+        Assert.assertEquals("Count check", 1, dao.count());
+        dao.delete(potato.getId());
+        Assert.assertEquals("Count check", 0, dao.count());
     }
 
 }
